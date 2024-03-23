b8776e0f1e9b9fa3cd3847d5444089dc04fcb795
konnokiyotaka@gmail.com <konnokiyotaka@gmail.com>
2024-03-23 16:47:55 +0900

    add layout file

---
+++ front/layouts/default.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+const route = useRoute();
+
+const navLinks = [
+  [],
+  [
+    {
+      label: "日本酒",
+      to: "/sakes",
+    },
+    {
+      label: "銘柄",
+      to: "/brands",
+    },
+    {
+      label: "酒蔵",
+      to: "/breweries",
+    },
+    {
+      label: "投稿",
+      to: "/comments",
+    },
+  ],
+  [
+    {
+      label: "Profile",
+      avatar: {
+        src: "https://avatars.githubusercontent.com/u/739984?v=4",
+      },
+    },
+  ],
+];
+
+const footerLinks = [
+  [],
+  [
+    {
+      label: "Code for SAKE",
+      to: "https://www.code4sake.org/",
+    },
+    {
+      label: "Github",
+      to: "https://github.com/Code-for-SAKE/Sakepedia-Nuxt",
+    },
+    {
+      label: "API仕様",
+      to: "/api/api-docs/",
+    },
+    {
+      label: "Sakepediaについて",
+      to: "/about",
+    },
+  ],
+  [],
+];
+</script>
+
+<template>
+  <div>
+    <UHorizontalNavigation :links="navLinks">
+      <template #default="{ link }">
+        <span class="group-hover:text-primary relative">{{ link.label }}</span>
+      </template>
+    </UHorizontalNavigation>
+    <slot />
+    <UHorizontalNavigation :links="footerLinks">
+      <template #default="{ link }">
+        <span class="group-hover:text-primary relative">{{ link.label }}</span>
+      </template>
+    </UHorizontalNavigation>
+  </div>
+</template>
+
---
+++ front/pages/brands/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <div class="d-flex justify-content-between align-items-center">
+      <h1>銘柄一覧</h1>
+      <UButton to="/brands/add">追加</UButton>
+    </div>
+  </div>
+</template>
