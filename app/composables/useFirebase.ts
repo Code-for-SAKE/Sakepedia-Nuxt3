99cb79caff292a0a862cc133acfc4e0cd1b9d68c
nichesuch <niche.and.such@gmail.com>
2024-06-22 17:06:12 +0900

    データ構造変更　mongodbからエクスポートしたサンプルデータを取り込み　firestoreのエミュレータにインポート

--- app/composables/useFirebase.ts
+++ app/composables/useFirebase.ts
@@ -31,59 +31,6 @@ export const useFirebaseApp = (): FirebaseApp => {
 
             const db = getFirestore(app);
             connectFirestoreEmulator(db, 'localhost', 8080);
-            // 酒蔵
-            setDoc(doc(db, "breweries", "60638e8e8d83689315fb5c1f"), {
-                name: "田中酒造",
-                kana: "たなかしゅぞう",
-                prefecture: "1",
-                address: "北海道小樽市色内３丁目２−５"
-            });
-            setDoc(doc(db, "breweries", "60638e8d8d83689315fb588b"), {
-                name: "EH酒造",
-                kana: "いえいちしゅぞう",
-                prefecture: "20",
-                address: "長野県安曇野市豊科高家1090－1"
-            });
-            setDoc(doc(db, "breweries", "60638e8e8d83689315fb5d23"), {
-                name: "桝田酒造店",
-                kana: "ますだしゅぞうてん",
-                prefecture: "",
-                address: "富山県富山市東岩瀬町269"
-            });
-            // 銘柄
-            setDoc(doc(db, "brands", "60638ea4a5c0139ca764747f"), {
-                name: "小樽の貴醸酒",
-                brewery: doc(db, "breweries", "60638e8e8d83689315fb5d23"),
-                description: ""
-            });
-            setDoc(doc(db, "brands", "60638ea4a5c0139ca7647480"), {
-                name: "亀甲蔵大吟醸",
-                brewery: doc(db, "breweries", "60638e8e8d83689315fb5c1f"),
-                description: ""
-            });
-            setDoc(doc(db, "brands", "60638ea4a5c0139ca7647485"), {
-                name: "大雪乃蔵",
-                brewery: doc(db, "breweries", "60638e8e8d83689315fb5d23"),
-                description: ""
-            });
-            setDoc(doc(db, "brands", "657e9c887da36bdc3c145f42"), {
-                name: "満寿泉",
-                brewery: doc(db, "breweries", "60638e8e8d83689315fb5c1f"),
-                description: "岩瀬では「岩泉」という銘柄で始めたが、港の芸者衆にも飲んでもらう為、昭和の始め頃今で言う 別ブランド展開で苗字の桝田にちなんだ｢満寿泉｣というまことにめでたい名をつけ販売した。 それが成功し、現在は「満寿泉」が主力ブランドになった。"
-            });
-            // 日本酒
-            setDoc(doc(db, "sakes", "657e9d9b7da36bdc3c146004"), {
-                name: "濃醇原酒「酔」",
-                brewery: doc(db, "breweries", "60638e8e8d83689315fb5d23"),
-                brand: doc(db, "brands", "657e9c887da36bdc3c145f42"),
-                description: "満寿泉の中で一番個性派で濃厚、飲み手を選ぶお酒。"
-            });
-            setDoc(doc(db, "sakes", "6270d0aba61c674723a05247"), {
-                name: "刈穂六舟吟醸酒",
-                brewery: doc(db, "breweries", "60638e8d8d83689315fb593a"),
-                brand: doc(db, "brands", "60638ea5a5c0139ca7647531"),
-                description: ""
-            });
 
             const functions = getFunctions(app);
             connectFunctionsEmulator(functions, "localhost", 5001);
---
+++ app/pages/dumper/index.vue
@@ -0,0 +1,90 @@
+<template>
+    <div>
+        <h1>インポート</h1>
+        <UButton class="success" @click="fetch_brewery">酒蔵</UButton><br>
+        <UButton class="success" @click="fetch_brand">銘柄</UButton><br>
+        <UButton class="success" @click="fetch_sake">日本酒</UButton><br>
+    </div>
+</template>
+
+
+<script setup>
+import { GeoPoint, getDoc, collection } from 'firebase/firestore';
+
+const { addItem, getReference } = useFirestore()
+
+const BASE_URL = "/data/";
+const HEADERS = {
+    "Content-Type" : "application/json"
+};
+
+const sleep = (time) => new Promise((resolve) => setTimeout(resolve, time));//timeはミリ秒
+
+
+const fetch_brewery = async () => {
+    const url = BASE_URL + `breweries.json`;
+    const brewery = await $fetch(url);
+    const breweries = brewery.split(/\n/)
+    console.log(breweries.length);
+    for(const text of breweries){
+        try{
+            if(text.length==0) break;
+            const bre = JSON.parse(text);
+            const b = bre;
+            b.location = new GeoPoint(b.latitude, b.longitude)
+            const id = bre._id["$oid"]
+            delete b._id
+            addItem("breweries", b, id);
+            await sleep(100);
+        }catch(e){
+            console.log(text, e)
+        }
+    }
+    console.log("done")
+};
+
+const fetch_brand = async () => {
+    const url = BASE_URL + `brands.json`;
+    const brand = await $fetch(url);
+    const brands = brand.split(/\n/)
+    console.log(brands.length);
+    for(const text of brands){
+        try{
+            if(text.length==0) break;
+            const data = JSON.parse(text)
+            const id = data._id["$oid"]
+            delete data._id
+
+            addItem("breweries/"+data.brewery["$oid"]+"/brands", data, id)
+            await sleep(100);
+        }catch(e){
+            console.log(text, e)
+        }
+    }
+    console.log("done")
+};
+
+
+
+const fetch_sake = async () => {
+    const url = BASE_URL + `sakes.json`;
+    const sake = await $fetch(url);
+    const sakes = sake.split(/\n/)
+    console.log(sakes.length);
+    for(const text of sakes){
+        try{
+            if(text.length==0) break;
+            const data = JSON.parse(text)
+
+            const id = data._id["$oid"]
+            delete data._id
+
+            addItem("breweries/"+data.brewery["$oid"]+"/brands/"+data.brand["$oid"]+"/sakes", data, id)
+            await sleep(100);
+        }catch(e){
+            console.log(text, e)
+        }
+    }
+    console.log("done")
+};
+</script>
