6f4a1265a190be54c70f78d9c057afcff6ff46ca
nichesuch <niche.and.such@gmail.com>
2024-07-14 14:15:34 +0900

    時刻をtimestamp型で持つように修正

--- app/pages/dumper/index.vue
+++ app/pages/dumper/index.vue
@@ -10,6 +10,7 @@
 
 <script setup>
 import { GeoPoint } from 'firebase/firestore';
+import dayjs from 'dayjs';
 
 const { setItem, getReference } = useFirestore()
 
@@ -29,12 +30,20 @@ const fetch_brewery = async () => {
     for(const text of breweries){
         try{
             if(text.length==0) break;
-            const bre = JSON.parse(text);
-            const b = bre;
-            b.location = new GeoPoint(b.latitude, b.longitude)
-            const id = bre._id["$oid"]
-            delete b._id
-            setItem("breweries", b, id);
+            const data = JSON.parse(text);
+            data.location = new GeoPoint(data.latitude, data.longitude)
+            const id = data._id["$oid"]
+            delete data._id
+
+            if(data.createdAt){
+                const createdAt = data.createdAt["$date"]
+                data.createdAt = dayjs(createdAt).toDate()
+            }
+            if(data.updatedAt){
+                const updatedAt = data.updatedAt["$date"]
+                data.updatedAt = dayjs(updatedAt).toDate()
+            }
+            setItem("breweries", id, data);
             await sleep(100);
         }catch(e){
             console.log(text, e)
@@ -57,6 +66,16 @@ const fetch_brand = async () => {
 
             const breweryId = data.brewery["$oid"]
             data.brewery = await getReference("breweries", breweryId);
+
+            if(data.createdAt){
+                const createdAt = data.createdAt["$date"]
+                data.createdAt = dayjs(createdAt).toDate()
+            }
+            if(data.updatedAt){
+                const updatedAt = data.updatedAt["$date"]
+                data.updatedAt = dayjs(updatedAt).toDate()
+            }
+
             setItem("breweries/"+breweryId+"/brands", id, data )
             await sleep(100);
         }catch(e){
@@ -86,6 +105,15 @@ const fetch_sake = async () => {
             
             const brandId = data.brand["$oid"];
             data.brand = await getReference("breweries/"+breweryId+"/brands/", breweryId);
+
+            if(data.createdAt){
+                const createdAt = data.createdAt["$date"]
+                data.createdAt = dayjs(createdAt).toDate()
+            }
+            if(data.updatedAt){
+                const updatedAt = data.updatedAt["$date"]
+                data.updatedAt = dayjs(updatedAt).toDate()
+            }
 
             setItem("breweries/"+breweryId+"/brands/"+brandId+"/sakes", id, data)
             await sleep(100);
