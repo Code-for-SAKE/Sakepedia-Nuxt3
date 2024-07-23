4984cf28091f896d4a177ca90c7b7c629f689369
konnokiyotaka@gmail.com <konnokiyotaka@gmail.com>
2024-07-23 17:30:03 +0900

    main反映

--- app/composables/useFirestore.ts
+++ app/composables/useFirestore.ts
@@ -1,44 +1,44 @@
 import type {
-    DocumentData,
-    DocumentReference,
-    DocumentSnapshot,
-    Query,
-    QueryDocumentSnapshot,
-    WithFieldValue,
+  DocumentData,
+  DocumentReference,
+  DocumentSnapshot,
+  Query,
+  QueryDocumentSnapshot,
+  WithFieldValue,
 } from "firebase/firestore"
 import {
-    getFirestore,
-    getCountFromServer,
-    getDoc,
-    setDoc,
-    addDoc,
-    deleteDoc,
-    doc,
-    collection,
-    query,
-    getDocs,
-    limit,
-    startAfter,
-    collectionGroup,
+  getFirestore,
+  getCountFromServer,
+  getDoc,
+  setDoc,
+  addDoc,
+  deleteDoc,
+  doc,
+  collection,
+  query,
+  getDocs,
+  limit,
+  startAfter,
+  collectionGroup,
 } from "firebase/firestore"
 import { useFirebaseApp } from "~/composables/useFirebase"
 
 export type Data<T> = {
-    id: string
-    path: string
-    ref: DocumentReference
-    data: T
+  id: string
+  path: string
+  ref: DocumentReference
+  data: T
 }
 
 export type Params<T> = {
-    query: Query
-    limit: number
-    before: T | undefined
-    converter: Converter<T>
+  query: Query
+  limit: number
+  before: T | undefined
+  converter: Converter<T>
 }
 export type Results<T> = {
-    list: Data<T>[]
-    listCount: number
+  list: Data<T>[]
+  listCount: number
 }
 
 export type Converter<T> = (snapshot: DocumentSnapshot<DocumentData, DocumentData>) => Data<T>
@@ -47,89 +47,89 @@ export type Converter<T> = (snapshot: DocumentSnapshot<DocumentData, DocumentDat
  * Firestore へのアクセス
  */
 export const useFirestore = () => {
-    const app = useFirebaseApp()
-    const db = getFirestore(app)
+  const app = useFirebaseApp()
+  const db = getFirestore(app)
 
-    const getSummary = async () => {
-        const brewery = await getCount(query(collection(db, "breweries")))
-        const brand = await getCount(query(collectionGroup(db, "brands")))
-        const sake = await getCount(query(collectionGroup(db, "sakes")))
-        const user = await getCount(query(collection(db, "users")))
-        const comment = await getCount(query(collection(db, "comments")))
-        return { brewery, brand, sake, user, comment }
+  const getSummary = async () => {
+    const brewery = await getCount(query(collection(db, "breweries")))
+    const brand = await getCount(query(collectionGroup(db, "brands")))
+    const sake = await getCount(query(collectionGroup(db, "sakes")))
+    const user = await getCount(query(collection(db, "users")))
+    const comment = await getCount(query(collection(db, "comments")))
+    return { brewery, brand, sake, user, comment }
+  }
+
+  const getCount = async (q: Query) => {
+    const listSnapshot = await getCountFromServer(q)
+    return listSnapshot.data().count
+  }
+
+  const getList = async <T>(params: Params<T>) => {
+    let q = params.query
+    const listSnapshot = await getCountFromServer(q)
+    if (params.before) {
+      q = query(q, startAfter(params.before))
     }
+    q = query(q, limit(params.limit))
+    const querySnapshot = await getDocs(q)
+    const listCount = listSnapshot.data().count
+    console.log("listCount", listCount)
 
-    const getCount = async (q: Query) => {
-        const listSnapshot = await getCountFromServer(q)
-        return listSnapshot.data().count
+    const list: Data<T>[] = []
+    querySnapshot.forEach((doc: QueryDocumentSnapshot<DocumentData, DocumentData>) => {
+      list.push(params.converter(doc))
+    })
+    console.log("list.length", list.length)
+
+    if (list.length) {
+      return {
+        list: list,
+        listCount: listCount,
+      } as Results<T>
     }
-
-    const getList = async <T>(params: Params<T>) => {
-        let q = params.query
-        const listSnapshot = await getCountFromServer(q)
-        if (params.before) {
-            q = query(q, startAfter(params.before))
-        }
-        q = query(q, limit(params.limit))
-        const querySnapshot = await getDocs(q)
-        const listCount = listSnapshot.data().count
-        console.log("listCount", listCount)
-
-        const list: Data<T>[] = []
-        querySnapshot.forEach((doc: QueryDocumentSnapshot<DocumentData, DocumentData>) => {
-            list.push(params.converter(doc))
-        })
-        console.log("list.length", list.length)
-
-        if (list.length) {
-            return {
-                list: list,
-                listCount: listCount,
-            } as Results<T>
-        }
-        return {
-            list: [],
-            listCount: listCount,
-        } as Results<T>
-    }
-
-    const getItem = async <T>(path: string, converter: Converter<T>) => {
-        const snapshot = await getDoc(doc(db, path))
-        return converter(snapshot)
-    }
-
-    const getReference = async (path: string) => {
-        const ref = doc(db, path)
-        return ref
-    }
-
-    const getFromReference = async (ref: DocumentReference) => {
-        return getDoc(ref)
-    }
-
-    const addItem = async <T extends WithFieldValue<DocumentData>>(path: string, params: T) => {
-        const coll = collection(db, path)
-        return await addDoc(coll, params)
-    }
-
-    const setItem = async <T extends WithFieldValue<DocumentData>>(path: string, params: T) => {
-        return await setDoc(doc(db, path), params)
-    }
-
-    const deleteItem = async (path: string) => {
-        return await deleteDoc(doc(db, path))
-    }
-
     return {
-        db,
-        getSummary,
-        getCount,
-        getList,
-        getItem,
-        getReference,
-        getFromReference,
-        addItem,
-        setItem,
-        deleteItem,
-    }
+      list: [],
+      listCount: listCount,
+    } as Results<T>
+  }
+
+  const getItem = async <T>(path: string, converter: Converter<T>) => {
+    const snapshot = await getDoc(doc(db, path))
+    return converter(snapshot)
+  }
+
+  const getReference = async (path: string) => {
+    const ref = doc(db, path)
+    return ref
+  }
+
+  const getFromReference = async (ref: DocumentReference) => {
+    return getDoc(ref)
+  }
+
+  const addItem = async <T extends WithFieldValue<DocumentData>>(path: string, params: T) => {
+    const coll = collection(db, path)
+    return await addDoc(coll, params)
+  }
+
+  const setItem = async <T extends WithFieldValue<DocumentData>>(path: string, params: T) => {
+    return await setDoc(doc(db, path), params)
+  }
+
+  const deleteItem = async (path: string) => {
+    return await deleteDoc(doc(db, path))
+  }
+
+  return {
+    db,
+    getSummary,
+    getCount,
+    getList,
+    getItem,
+    getReference,
+    getFromReference,
+    addItem,
+    setItem,
+    deleteItem,
+  }
 }
