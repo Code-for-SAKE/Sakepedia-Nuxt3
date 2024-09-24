142f2010a4227b59ce682f97b559d01e58d46844
konnokiyotaka@gmail.com <konnokiyotaka@gmail.com>
2024-09-24 21:30:25 +0900

    日本酒詳細ページからの醸造データ追加ページ呼び出し #46

--- app/pages/breweries/[breweryId]/brands/[brandId]/sakes/[sakeId]/index.vue
+++ app/pages/breweries/[breweryId]/brands/[brandId]/sakes/[sakeId]/index.vue
@@ -2,50 +2,78 @@
 import type { Data } from "~/composables/useFirestore"
 
 const route = useRoute()
-const { getItem, deleteItem } = useSake()
+const { getItem: getSake, deleteItem } = useSake()
+const { getItem: getBrewery } = useBrewery()
+const { getItem: getBrand } = useBrand()
 const { defaultLocale } = useI18n()
 const localePath = useLocalePath()
 const dataPath = localePath(route.path, defaultLocale)
 
-const item = await getItem(dataPath)
-const sake: Data<Sake> = item!
+const item = await getSake(dataPath)
+const sake: Data<Sake> = item
 
-console.log("Sakeitem", item)
+const breweryPath = sake.data.brewery?.path
+const brewery = await getBrewery(breweryPath)
+const brandPath = sake.data.brand?.path
+const brand = await getBrand(brandPath)
 
 const confirmDelete = ref(false)
-const deleteSake = async function () {
-  deleteItem(item.path)
+async function deleteRecord() {
+  await deleteItem(dataPath)
+  await navigateTo(localePath("/" + sake?.data.brand?.path))
 }
 </script>
 
 <template>
   <div>
-    <h1>{{ $t("sakeDetails") }}</h1>
+    <h1>{{ $t("sake") }}</h1>
     <hr />
     <h2 v-if="sake">{{ sake?.data.name }}</h2>
     <dl>
       <dt>{{ $t("brewery") }}</dt>
       <dd>
         <NuxtLink :to="localePath('/' + sake?.data.brewery?.path)">
-          <div class="w-full">{{ sake?.data.brewery?.id }}</div>
+          <div class="w-full">{{ brewery.data.name }}</div>
         </NuxtLink>
       </dd>
       <dt>{{ $t("brand") }}</dt>
       <dd>
         <NuxtLink :to="localePath('/' + sake?.data.brand?.path)">
-          <div class="w-full">{{ sake?.data.brand?.id }}</div>
+          <div class="w-full">{{ brand.data.name }}</div>
         </NuxtLink>
       </dd>
       <dt>{{ $t("type") }}</dt>
-      <dd />
+      <dd>
+        <h3>
+          <nuxt-link
+            v-for="type in sake.data.type"
+            :key="type"
+            :to="'/sakes?type=' + type"
+            class="badge badge-pill badge-primary p-2 m-1"
+            >{{ type }}</nuxt-link
+          >
+        </h3>
+      </dd>
       <dt>{{ $t("pairing") }}</dt>
-      <dd />
+      <dd>
+        <h3>
+          <nuxt-link
+            v-for="mariages in sake.data.mariages"
+            :key="mariages"
+            :to="'/sakes?type=' + mariages"
+            class="badge badge-pill badge-primary p-2 m-1"
+            >{{ mariages }}</nuxt-link
+          >
+        </h3>
+      </dd>
       <dt>{{ $t("explanation") }}</dt>
       <dd>{{ sake?.data.description }}</dd>
       <dt>{{ $t("url") }}</dt>
-      <dd />
+      <dd>
+        <a v-if="sake.data.url" :href="sake.data.url">{{ sake.data.url }}</a>
+      </dd>
       <dt>{{ $t("updatedAt") }}</dt>
-      <dd />
+      <dd>{{ sake.data.updatedAt }}</dd>
     </dl>
     <UButton class="info" :to="localePath(route.path + '/update')">{{ $t("edit") }}</UButton>
     <UButton class="danger" @click="confirmDelete = true">{{ $t("delete") }}</UButton>
@@ -63,18 +91,19 @@ const deleteSake = async function () {
       <div class="d-flex justify-content-between align-items-center">
         <h3>{{ $t("latestBreweryData") }}</h3>
       </div>
-      <last-brewery-year-data :sake="$route.params.id" class="m-3" />
+      <LastBreweryYearSpec :sake="sake" class="m-3" />
       <hr />
       <div class="d-flex justify-content-between align-items-center">
         <h3>{{ $t("otherData") }}</h3>
         <div class="d-flex justify-content-between">
           <div></div>
-          <UButton class="success" @click="confirmDelete = false">{{
+          <UButton class="info" :to="localePath(route.path + '/breweryYearSpecs/add')">{{
             $t("addBreweryData")
           }}</UButton>
         </div>
       </div>
       <!-- <brewery-year-data-list class="m-3" /> -->
+      <BreweryYearSpecList :sake="sake" />
     </div>
     <div class="my-4">
       <div class="d-flex justify-content-between align-items-center">
